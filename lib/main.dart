bfe2e471e4d598ee3f6ad18a421e5273d6a453f7
skyhackeR <jolayemidavid6@gmail.com>
2024-05-09 17:01:29 +0100

    Adding of images and gifs

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,80 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
+class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: ' Demo Page'),
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class MainPage extends StatefulWidget {
+  _MainPageState createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  final Random random = Random();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // List of image URLs for placeholders
+  final List<String> imageUrls = [
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/200',
+    'https://via.placeholder.com/250',
+    'https://via.placeholder.com/300',
+    'https://via.placeholder.com/350',
+  ];
+
+  // List of GIF URLs
+  final List<String> gifUrls = [
+    'https://media.giphy.com/media/l4FGo4yXeJMeBRNLO/giphy.gif',
+    'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif',
+    'https://media.giphy.com/media/3o6fJ8jngfVrulDK4o/giphy.gif',
+  ];
+
+  // Generate random data with mixed types
+  List<dynamic> _generateRandomData(int count) {
+    List<dynamic> data = [];
+    for (int i = 0; i < count; i++) {
+      int choice = random.nextInt(3);
+      switch (choice) {
+        case 0:
+          // Add random image
+          data.add(Image.network(imageUrls[random.nextInt(imageUrls.length)]));
+          break;
+        case 1:
+          // Add random animation
+          data.add(Icon(Icons.favorite, color: Colors.red));
+          break;
+        case 2:
+          // Add random GIF
+          data.add(Image.network(gifUrls[random.nextInt(gifUrls.length)]));
+          break;
+      }
+    }
+    return data;
   }
 
   @override
   Widget build(BuildContext context) {
+    // Generate random data for the ListView
+    List<dynamic> items = _generateRandomData(10);
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('My App'),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(child: items[index]),
+          );
+        },
       ),
     );
   }
